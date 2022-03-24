74f1aeecde9bd4949ea1c817d4a192d4437559ab
Vaishnavi-N-08 <vaishnavinarkhede8@gmail.com>
2022-03-24 11:32:28 +0530

    mailing done

--- spices/views.py
+++ spices/views.py
@@ -1,10 +1,13 @@
 
+from email.message import EmailMessage
 from multiprocessing import context
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.core.mail import send_mail
-
+import threading
+from threading import Thread
 from spices.models import Cart, CartItem, Product
+from website.settings import EMAIL_HOST_USER
 
 # async def prvai():
 #     for i in range(1, 100):
@@ -13,6 +16,27 @@ from spices.models import Cart, CartItem, Product
 #         await asyncio.sleep(1)
 #     return True
         
+# send mail function
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        threading.Thread.__init__(self)
+
+    def run (self):
+        # msg = EmailMessage(self.subject, self.html_content, EMAIL_HOST_USER, self.recipient_list)
+        # msg.content_subtype = "html"
+        # msg.send()
+
+        send_mail(self.subject, self.html_content, EMAIL_HOST_USER, self.recipient_list, fail_silently=False)
+
+def send_html_mail(subject, html_content, recipient_list):
+    EmailThread(subject, html_content, recipient_list).start()
+
+
 
 # Create your views here.
 @login_required(login_url='/register/')
@@ -29,6 +53,7 @@ def spices(request):
                 cartitem = CartItem(cart=cart, product=i, quantity=tar)
                 cartitem.save()
                 total += int(tar) * i.price
+        send_html_mail('Order Placed', '<h1>Order Placed</h1>', ['om.surushe20@vit.edu'])
         flag = True
         context = {'flag':flag, 'total':total}
         return render(request, 'spices.html', context)
--- website/settings.py
+++ website/settings.py
@@ -130,6 +130,18 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media directory in the root directory
 
+# AUTH_USER_MODEL = 'User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'vishwasocialmail@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = "12010357"
+
+
+
+
 # JET_DEFAULT_THEME = 'green'
 
 # JET_SIDE_MENU_COMPACT = True
