b46ee975a6933983c751a63b6bf70c705bd6ed1e
om-surushe <83531291+om-surushe@users.noreply.github.com>
2022-03-27 03:47:00 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['aarshitigroup.in']
+ALLOWED_HOSTS = ['aarshitigroup.in','*',107.22.24.149]
 
 
 # Application definition
